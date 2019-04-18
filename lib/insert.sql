5a56e9b72e33c7e003fe2e1700dde4e96e37271b
Arthur Song <arth3rs0ng@gmail.com>
2019-04-18 20:36:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "male" "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "male", "brown", "angry", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "pink", "docile", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha" 15, "female", "tan", "playful", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 37, "male", "dark brown", "strict", 1);
