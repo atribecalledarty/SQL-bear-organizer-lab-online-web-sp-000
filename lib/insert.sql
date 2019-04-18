ccff569ee351bd61fbab5e35e2efc5fb6a881ec5
Arthur Song <arth3rs0ng@gmail.com>
2019-04-18 20:32:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,5 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha" "15", "female", "tan", "playful", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "37", "male", "dark brown", "strict", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "19", "female", "red", "active", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "19", "female", "red", "active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", "59", "male", "green", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "9", "female", "orange", "friendly", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "19", "female", "red", "active", 1);
